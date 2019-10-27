3a92f15fcc76992c59d62637e0f3ddd71f28c141
Tareq_N <tareq.pro.1996@gmail.com>
2019-10-27 20:21:54 +0300

    edit student

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -4,33 +4,37 @@ import { Table } from 'reactstrap';
 import { Input } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import Axios from 'axios';
 
 
 class StudentPage extends Component {
-    state = {
-        name: 'aas asf',
-        phone1: '',
-        phone2: '',
-        status: '',
-        CPAID: '',
-        CPABalance: '',
-        terms: '',
-        remarks: ''
-    }
-
-    handleChange(e) {
-        this.setState({
-            name: e.target.value,
-        });
-    }
-
     constructor(props) {
         super(props);
+
         this.state = {
+            name: null,
+            phone: '',
+            phone2: '',
+            status: '',
+            CPA: '',
+            CPABalance: '',
+            terms: '',
+            remarks: '',
+            specialty: '',
             modal: false
         };
 
         this.toggle = this.toggle.bind(this);
+
+    }
+
+    handleChange(e) {
+        let item = [e.target.id]
+        let value = e.target.value;
+
+        this.setState({
+            [item]  : value
+        })
     }
 
     toggle() {
@@ -39,20 +43,41 @@ class StudentPage extends Component {
         }));
     }
 
+    handleEdit = () => {
+        console.log(this.state);
+
+        Axios.post(`http://localhost:9000/students/api/v1/students/edit/${this.props.match.params['id']}`, {
+            "student": {
+                "name": this.state.name,
+                "phone1": this.state.phone,
+                "phone2": this.state.phone2,
+                "cpa": this.state.CPA, 
+                "balance": this.state.CPABalance
+            }
+        }).then(res => console.log(res))
+        .catch(error => console.error(error))
+    }
+
     render() {
         const activeGroups = this.props.activeGroups
         let student;
         // let groups = []
         if (this.props.student) {
             student = this.props.student
-            // groups = Object.values(student.groups)
+            if (!this.state.name) {
+                this.setState({
+                    name: student.name,
+                    phone: student.phone,
+                    phone2: student.phone2,
+                    CPA: student.CPA,
+                    CPABalance: student.CPABalance
+                })
+            }
+
         } else {
             student = {}
         }
 
-
-
-        // console.log('student=', student)
         return (
             <div className='student-page container my-4 py-4 shadow'>
                 <h3 className='col-12 '>{student.name} </h3>
@@ -60,9 +85,9 @@ class StudentPage extends Component {
                 <br />
                 <div className='row margin-top-7 '>
                     <div className='col-2 mb-3 '><span className='gray'>ID: </span>  <span>{student.id}</span></div>
-                    <div className='col-10 mb-3'><span className='gray '>Name: </span><Input className='float-left form-control-sm' type="text" value={student.name} onChange={(e) => this.handleChange(e)} /> </div>
-                    <div className='col-12 col-md-6  mb-3'><span className='gray '>Phone 1: </span><Input className='float-left form-control-sm' type="text" value={student.phone} onChange={(e) => this.handleChange(e)} /> </div>
-                    <div className='col-12 col-md-6  mb-3'><span className='gray '>Phone 2: </span><Input className='float-left form-control-sm' type="text" value={student.phone2} onChange={(e) => this.handleChange(e)} /></div>
+                    <div className='col-10 mb-3'><span className='gray '>Name: </span><Input className='float-left form-control-sm' type="text" id="name" value={this.state.name} onChange={(e) => this.handleChange(e)} /> </div>
+                    <div className='col-12 col-md-6  mb-3'><span className='gray '>Phone 1: </span><Input className='float-left form-control-sm' id="phone" type="text" value={this.state.phone} onChange={(e) => this.handleChange(e)} /> </div>
+                    <div className='col-12 col-md-6  mb-3'><span className='gray '>Phone 2: </span><Input className='float-left form-control-sm' id="phone2" type="text" value={this.state.phone2} onChange={(e) => this.handleChange(e)} /></div>
                     <div className='col-12 mb-3'>
                         <span className='gray'>Status:</span>
                         <Input className='form-control-sm' type="select" name="select" id="exampleSelect">
@@ -70,10 +95,11 @@ class StudentPage extends Component {
                             <option defaultValue>finish</option>
                         </Input>
                     </div>
-                    <div className='col-12 col-md-2 mb-3'><span className='gray '>CPA:</span><Input className='float-left form-control-sm' type="text" value={student.CPA} onChange={(e) => this.handleChange(e)} /></div>
-                    <div className='col-12 col-md-8 mb-3'><span className='gray'>CPA Balance: </span><Input className='float-left form-control-sm' type="text" value={student.CPABalance} onChange={(e) => this.handleChange(e)} /></div>
+                    <div className='col-12 col-md-2 mb-3'><span className='gray '>CPA:</span><Input className='float-left form-control-sm' type="text" id="CPA" value={this.state.CPA} onChange={(e) => this.handleChange(e)} /></div>
+                    <div className='col-12 col-md-8 mb-3'><span className='gray'>CPA Balance: </span><Input className='float-left form-control-sm' id="CPABalance" type="text" value={this.state.CPABalance} onChange={(e) => this.handleChange(e)} /></div>
                     <div className='col-12 col-lg-3 mb-3'> <span className='gray'>sign up date: </span> <span>{student.creationDate}</span></div>
                     <div className='col-12 col-lg-9 mb-3'><span className='gray'>last date: </span> <span>{student.lastDate}</span></div>
+                    <Button className="ml-3" onClick={() => this.handleEdit()}>Edit</Button>
 
 
                     <div className='col-12 mt-4 scrollabel-container'>
@@ -183,7 +209,6 @@ function mapStateToProps({ students, groups, studentsGroups }, props) {
         });
     }
 
-    // console.log('studentGroups filters',studentGroups)
     return {
         student,
         students,
